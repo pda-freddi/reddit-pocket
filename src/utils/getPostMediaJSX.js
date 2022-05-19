316018b0c97d2787313993e4ac56d6c9d95f52ab
Pedro Freddi <pdafreddi@gmail.com>
2022-05-19 17:28:11 -0300

    Adjust indentation in getPostMedia.js

--- src/utils/getPostMediaJSX.js
+++ src/utils/getPostMediaJSX.js
@@ -5,30 +5,38 @@ import Markdown from "markdown-to-jsx";
 export function getPostMediaJSX(post) {
 
   if (post.postHint === 'image') {
-    return <img src={post.url} alt="" className={styles.postImage} />;
+    return (
+      <img src={post.url} alt="" className={styles.postImage} />
+    );
   }
 
   if (post.isVideo) {
-    return (<video 
-    controls 
-    src={post.media.fallback_url}
-    className={styles.postVideo}>
-  </video>);
+    return (
+      <video 
+      controls 
+      src={post.media.fallback_url}
+      className={styles.postVideo}>
+      </video>
+    );
   }
 
   if (post.text) {
-    return (<Markdown className={styles.postText}>{post.text}</Markdown>);
+    return (
+      <Markdown className={styles.postText}>{post.text}</Markdown>
+    );
   }
 
   if (post.postHint === "link" || post.postHint === "rich:video" || 
   (post.postHint === undefined && !post.isVideo && !post.text && !post.url.includes("comments"))) {
-    return (<div className={styles.postExternalLinkContainer}>
-    <a 
-      href={post.url} 
-      target="_blank" 
-      className={styles.postExternalUrl} 
-      rel="noreferrer">Link to external resource</a>
-    <img src={post.thumbnail.url} alt="" className={styles.postThumbnail} />
-  </div>);
+    return (
+      <div className={styles.postExternalLinkContainer}>
+        <a 
+        href={post.url} 
+        target="_blank" 
+        className={styles.postExternalUrl} 
+        rel="noreferrer">Link to external resource</a>
+        <img src={post.thumbnail.url} alt="" className={styles.postThumbnail} />
+      </div>
+    );
   }
 }
